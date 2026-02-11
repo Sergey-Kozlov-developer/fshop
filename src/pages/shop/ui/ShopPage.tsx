c6255b72961915b81fd6f267c4f8dcef3af6bf59
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2026-02-11 15:14:04 +0300

    Add promo card ui

--- src/pages/shop/ui/ShopPage.tsx
+++ src/pages/shop/ui/ShopPage.tsx
@@ -1,9 +1,13 @@
+import PromoCards from "@/widgets/promo-cards/promoCards";
 import SwiperSlider from "@/widgets/slider/slider";
 
 const ShopPage = () => {
     return (
         <div className="w-full">
             <SwiperSlider />
+            <div className="container mx-auto mt-32 mb-24">
+                <PromoCards />
+            </div>
         </div>
     );
 };
---
+++ src/widgets/promo-cards/promoCards.tsx
@@ -0,0 +1,53 @@
+import promoOne from "@/shared/assets/image/promo-card/promo-1.png";
+import promoTwo from "@/shared/assets/image/promo-card/promo-2.png";
+
+const PromoCards = () => {
+    const promoCard = [
+        {
+            id: 1,
+            img: promoOne,
+            article: "Low Price",
+            title: "High Coziness",
+            promoPrice: "UPTO 50% OFF",
+            linkItem: "Explore Items",
+        },
+        {
+            id: 2,
+            img: promoTwo,
+            article: "Beyoung Presents",
+            title: "Breezy Summer Style",
+            promoPrice: "UPTO 50% OFF",
+            linkItem: "Explore Items",
+        },
+    ];
+
+    return (
+        <div className="grid justify-between md:grid-cols-2 sm:grid-cols-1 gap-x-7 sm:gap-y-7 max-sm:gap-y-7">
+            {promoCard.map((item) => (
+                <div key={item.id} className="relative max-sm:mx-5 ">
+                    <img
+                        className="rounded-xl shadow-[2px_2px_10px_2px_rgba(0,0,0,0.40)]"
+                        src={item.img}
+                        alt="Promo 1"
+                    />
+                    <div className="absolute justify-start max-sm:top-4 sm:top-16 left-7 ">
+                        <p className="mb-6 text-white text-lg font-extrabold font-['Montserrat'] leading-8 tracking-tight">
+                            {item.article}
+                        </p>
+                        <p className="mb-2 text-white text-4xl font-extrabold font-['Core_Sans_C'] leading-10">
+                            {item.title}
+                        </p>
+                        <p className="max-sm:mb-5 sm:mb-10  text-white text-base font-medium font-['Core_Sans_C'] leading-6 tracking-tight">
+                            {item.promoPrice}
+                        </p>
+                        <p className=" text-white text-xl font-extrabold font-['Core_Sans_C'] underline leading-8">
+                            {item.linkItem}
+                        </p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default PromoCards;
